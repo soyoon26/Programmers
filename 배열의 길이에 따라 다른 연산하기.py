cf3b8715ebadae83d221fb4e584af3f3470ae39c
hangsangharr <122415737+soyoon26@users.noreply.github.com>
2023-12-20 21:08:56 +0900

    2023/12/20

---
+++ 배열의 길이에 따라 다른 연산하기.py
@@ -0,0 +1,11 @@
+# 정수 배열 arr과 정수 n이 매개변수로 주어집니다. arr의 길이가 홀수라면 arr의 모든 짝수 인덱스 위치에 n을 더한 배열을, arr의 길이가 짝수라면 arr의 모든 홀수 인덱스 위치에 n을 더한 배열을 return 하는 solution 함수를 작성해 주세요.
+
+def solution(arr, n):
+    if len(arr) % 2 == 0 :
+        for i in range(0,len(arr)//2):
+            arr[i*2+1] = arr[i*2+1]+n
+    else:
+        for i in range(0,(len(arr)+1)//2):
+            arr[i*2] = arr[i*2]+n
+    answer = arr
+    return answer
