3653642580bfa262780e061cbe6b68db439ce2b9
hangsangharr <122415737+soyoon26@users.noreply.github.com>
2023-12-21 18:12:08 +0900

    2023/12/21

---
+++ 중복된 숫자 개수.py
@@ -0,0 +1,8 @@
+# 정수가 담긴 배열 array와 정수 n이 매개변수로 주어질 때, array에 n이 몇 개 있는 지를 return 하도록 solution 함수를 완성해보세요.
+
+def solution(array, n):
+    answer = 0
+    for i in range(len(array)):
+        if n == array[i]:
+            answer += 1
+    return answer
