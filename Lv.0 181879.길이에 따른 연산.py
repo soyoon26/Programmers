ff15321e8be65df4da7e75d689b697079fdf332b
isang <barcardi26@gmail.com>
2024-08-31 23:57:30 +0900

    2024/08/31

---
+++ Lv.0 181879.길이에 따른 연산.py
@@ -0,0 +1,10 @@
+def solution(num_list):
+    answer = 1
+    if len(num_list) >= 11:
+        answer = sum(num_list)
+    else:
+        for i in range(len(num_list)):
+            answer*=num_list[i]
+    return answer
+
+# prod 사용
